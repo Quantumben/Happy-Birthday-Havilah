d4316dd6b318c2ec3183fe755a57b2c07847052e
Benedict Goodluck <omosabenedict@gmail.com>
2024-06-30 09:55:17 -0700

    Add files via upload
    Happy Birthday PrincessHavy

---
+++ Happy_Birthday.py
@@ -0,0 +1,9 @@
+def happy_birthday(name):
+    print(f"Happy Birthday, {name}!")
+    print("Wishing you all the best on your special day!")
+    print("May your year be filled with joy, health, and success.")
+    print("\nHere are some virtual balloons for you 🎈🎈🎈")
+    
+# Replace 'Havilah' with the name of the person you want to wish
+name = "Havilah"
+happy_birthday(name)
